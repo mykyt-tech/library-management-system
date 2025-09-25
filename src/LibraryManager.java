958509573f54ef092282d25a3d8cf1d20966abaa
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-25 16:29:36 +0200

    feat: add addNewMember method to LibraryManager

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -3,4 +3,8 @@ import java.util.ArrayList;
 public class LibraryManager {
     private static final ArrayList<Book> books = new ArrayList<>();
     private static final ArrayList<Member> members = new ArrayList<>();
+
+    public static void addNewMember(int id, String name) {
+        members.add(new Member(id, name));
+    }
 }
