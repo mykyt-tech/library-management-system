8698d51569842faea305d5408e16cb473637d0b9
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-25 16:40:00 +0200

    feat: add addNewBook method to LibraryManager

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -7,4 +7,8 @@ public class LibraryManager {
     public static void addNewMember(int id, String name) {
         members.add(new Member(id, name));
     }
+
+    public static void addNewBook(String title, String author, String isbn, int availableCopies) {
+        books.add(new Book(title, author, isbn, availableCopies));
+    }
 }
