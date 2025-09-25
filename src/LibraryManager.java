3c7b7f5b13b4386344dc449ce4f2bbd7f3de21e2
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-25 16:24:07 +0200

    feat: add ArrayList to store books and ArrayList to store members to LibraryManager

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -1,3 +1,6 @@
-public class LibraryManager {
+import java.util.ArrayList;
 
+public class LibraryManager {
+    private static final ArrayList<Book> books = new ArrayList<>();
+    private static final ArrayList<Member> members = new ArrayList<>();
 }
