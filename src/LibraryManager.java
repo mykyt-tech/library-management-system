22a8d67996e61f20c7d24446c624f8a8032662be
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-25 16:49:26 +0200

    feat: add removeBook method to LibraryManager

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -11,4 +11,17 @@ public class LibraryManager {
     public static void addNewBook(String title, String author, String isbn, int availableCopies) {
         books.add(new Book(title, author, isbn, availableCopies));
     }
+
+    public static void removeBook(String isbn) {
+        for (Member m : members) {
+            for (Book b : m.getBorrowedBooks()) {
+                if (b.getIsbn().equals(isbn)) {
+                    System.out.println("Cannot remove book: currently borrowed by member(" + m.getId() + ").");
+                    return;
+                }
+            }
+        }
+
+        books.removeIf(b -> b.getIsbn().equals(isbn));
+    }
 }
